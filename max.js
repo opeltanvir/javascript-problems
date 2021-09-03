ec127cee88d00449aefa11ef9fab5bcd97313f66
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2021-09-03 22:21:08 +0600

    upload

---
+++ max.js
@@ -0,0 +1,24 @@
+var business = 450;
+var minister = 650;
+var sochib = 550;
+
+var max = Math.max(business,minister,sochib)
+console.log(max);
+/** 
+if(business > minister){
+    if(business > sochib){
+         console.log('business is bigger');
+    }
+}
+else{
+    console.log('sochib is bigger');
+}
+  
+else{
+    if(minister > sochib){
+        console.log('minister is bigger');
+    }
+    else{
+        console.log('minister is bigger');
+    }
+*/
