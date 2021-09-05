edc293ce8c177d71356f0ab03878535e6c553003
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2021-09-05 16:30:12 +0600

    upload

---
+++ animal.js
@@ -0,0 +1,21 @@
+function animaleCalculator (depth){
+    var animal = 0;
+    if (depth <= 10) {
+        animal = depth * 50;
+    }else if (depth <= 20){
+        var firstpart = 10 * 50;
+        var remaining = depth - 10;
+        var secondpart = remaining *100;
+        animal = firstpart + secondpart;
+    }
+    else{
+        var firstpart = 10 *50;
+        var secondpart = 10*100;
+        var remaining = depth - 20;
+        var thirdpart = remaining * 300;
+        animal = firstpart + secondpart + thirdpart ;
+    }
+    return animal;
+}
+var count = animaleCalculator(22);
+console.log(count);
